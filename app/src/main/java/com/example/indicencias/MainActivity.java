9ae5a98808b3ec9695d0d33fe61be0791cf67c64
piter <pjmolina2012@gmail.com>
2020-10-13 23:14:00 -0400

    segundo Toast

--- app/src/main/java/com/example/indicencias/MainActivity.java
+++ app/src/main/java/com/example/indicencias/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hola GITHUB", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "VAMOS seguimos", Toast.LENGTH_SHORT).show();
     }
 }
